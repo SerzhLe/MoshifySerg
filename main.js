d949fccf3129a3a2ff99e16457e206edf5afee72
Sergey Leshchenko <sergserg57@ukr.net>
2022-04-09 11:27:17 +0300

    Create blocks of site, make changes in domain-list

--- main.js
+++ main.js
@@ -4,3 +4,17 @@ collapsibles.forEach((item) =>
     this.classList.toggle("collapsible--expanded");
   })
 );
+
+const badges = document.querySelectorAll(".badges");
+badges.forEach((item) =>
+  item.addEventListener("click", function () {
+    this.classList.toggle("badge");
+  })
+);
+
+const badgesSecondary = document.querySelectorAll(".badges");
+badgesSecondary.forEach((item) =>
+  item.addEventListener("click", function () {
+    this.classList.toggle("badge--secondary");
+  })
+);
